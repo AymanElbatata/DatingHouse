d35573d510f468b2de11626ab02e10e621da20d2
Ayman (Айман ЭльБатата) Elbatata <92555608+AymanElbatata@users.noreply.github.com>
2025-10-17 21:22:55 +0300

    update
    update

--- AYMDatingCore.PL/AYMDatingCore.BLL/obj/Debug/net9.0/AYMDatingCore.BLL.AssemblyInfo.cs
+++ AYMDatingCore.PL/AYMDatingCore.BLL/obj/Debug/net9.0/AYMDatingCore.BLL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AYMDatingCore.BLL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fbedc6f3781d4c4842064dbc812b58cc35446f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("AYMDatingCore.BLL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AYMDatingCore.BLL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- AYMDatingCore.PL/AYMDatingCore.DAL/obj/Debug/net9.0/AYMDatingCore.DAL.AssemblyInfo.cs
+++ AYMDatingCore.PL/AYMDatingCore.DAL/obj/Debug/net9.0/AYMDatingCore.DAL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AYMDatingCore.DAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fbedc6f3781d4c4842064dbc812b58cc35446f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("AYMDatingCore.DAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AYMDatingCore.DAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
